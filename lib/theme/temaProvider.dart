a82a45540f98992c445a8fa0e2fa02a867bcc72a
ibrahim <btoytekin80@gmail.com>
2023-04-09 14:12:35 +0300

    biz sizi arariz

---
+++ lib/theme/temaProvider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TemaProvider with ChangeNotifier {
+  int _temaSec = 0;
+
+  int get count => _temaSec;
+
+  void beyazTeme() {
+    _temaSec = 0;
+    notifyListeners();
+  }
+
+  void siyahTema() {
+    _temaSec = 1;
+    notifyListeners();
+  }
+}
